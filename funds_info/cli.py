5364fb5b19c7e48d8691819c757769b6e3ac2a35
Kirill Fertikov <kirill.fertikov@gmail.com>
2022-11-06 00:02:50 +0500

    cli fixes

--- funds_info/cli.py
+++ funds_info/cli.py
@@ -1,12 +1,16 @@
 import argparse
 import json
 
-if __name__ == '__main__':
-    from __init__ import get
+from funds_info import get
 
+
+def main():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('instruments', metavar='instrument', type=str, nargs='+',
                             help='an instrument to get info for')
     args = vars(arg_parser.parse_args())
-
     print(json.dumps(get(args['instruments']), indent=2, ensure_ascii=False))
+
+
+if __name__ == '__main__':
+    main()
