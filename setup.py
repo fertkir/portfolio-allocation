6b1c29b6c3ffff26997f0ab9a2bb821fd6fbcce0
Kirill Fertikov <kirill.fertikov@gmail.com>
2022-11-08 12:46:51 +0500

    hyphen -> underscore in package name

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name="portfolio-allocation",  # Required
+    name="portfolio_allocation",  # Required
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
     #
