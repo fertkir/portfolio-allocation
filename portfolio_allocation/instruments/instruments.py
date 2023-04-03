e896d9c656e45d600b73fea82547437888855537
Kirill Fertikov <kirill.fertikov@gmail.com>
2023-04-04 00:57:21 +0500

    small fix

--- portfolio_allocation/instruments/instruments.py
+++ portfolio_allocation/instruments/instruments.py
@@ -1,11 +1,7 @@
-from .sources import currencies, funds, securities
+from .sources import currencies, funds, securities, custom
 
 
 def get_data(tickers: list[str]) -> dict[str, dict]:
-    # custom_info = custom.custom(tickers)
-    # _custom = custom_info.keys()
-    # todo should not filter out if no parameters are set in custom config:
-    # not_custom = list(filter(lambda ticker: ticker not in _custom, tickers))
     info_by_currency = currencies.currencies(tickers)
     _currencies = info_by_currency.keys()
     not_currencies = list(filter(lambda ticker: ticker not in _currencies, tickers))
@@ -14,6 +10,8 @@ def get_data(tickers: list[str]) -> dict[str, dict]:
     other = list(filter(lambda ticker: ticker not in _funds, not_currencies))
     _securities = securities.securities(other)
     result = info_by_currency | info_by_fund | _securities
-    # for key, value in result.items():
-    #     for
+    custom_info = custom.custom(tickers)
+    for key, value in result.items():
+        if key in custom_info.keys():
+            value.update(custom_info[key])
     return result
